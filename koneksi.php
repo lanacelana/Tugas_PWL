29f061e6d66f658a87b90f536f73ed904ad3d4cd
lanacelana <47437630+lanacelana@users.noreply.github.com>
2022-08-24 16:16:31 +0700

    Update koneksi.php

--- koneksi.php
+++ koneksi.php
@@ -1,10 +1,10 @@
 <?php 
 $servername = "localhost";
 $user		= "root";
-$password	= "BidPd4!!";
-$db			= "perpus";
+$password	= "";
+$db		= "perpus";
 
-$conn = mysqli_connect ("localhost","root","B1dPd4!!","perpus")
+$conn = mysqli_connect ("localhost","root","","perpus")
 			or die ('gagal terkoneksi'.mysqli_connect_error());
 			
 //$database = mysqli_select_db ($db)
